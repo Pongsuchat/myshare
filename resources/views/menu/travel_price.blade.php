e4337101e759f54e7967489c2973dd6743bbf4ff
Jirawat Chaiyawong <darkwat4.8@gmail.com>
2020-07-29 18:06:58 +0700

    แก้ไขหน้าเรทราคา

--- resources/views/menu/travel_price.blade.php
+++ resources/views/menu/travel_price.blade.php
@@ -20,28 +20,139 @@
   <div class="col-md-10 shadow p-3  rounded">
 
     <nav class="navbar navbar-light shadow " style="background-color: #b8e8ee;margin-bottom: 1em;">
-      <a class="navbar-brand">ข้อมูลผู้ใช้งาน</a>
+      <a class="navbar-brand">แก้ไขเรทราคาของ Trip ทั้งหมด</a>
     </nav>
 
-    <div class="contrainer">
-      <form class="form-inline" method="POST" action="{{url('/tripprice')}}">
-        {{csrf_field()}}
-        <div class="form-group mb-2">
-          @if (!empty($pricerate))
-          <a>เรทราคาของ Trip ปัจจุบัน = {{$pricerate}} ต่อระยะทาง 1 กิโลเมตร</a>
+
+    <div class="price-rate-card bg-light">
+      <div class="card-body text-center">
+        <p class="price-rate-title">เรทราคา Trip เดินทาง</p>
+        <div class="">
+          <form class="form-inline" style="margin-bottom: 5px !important;" method="POST" action="{{url('/tripprice')}}">
+            {{csrf_field()}}
+            <div class="form-group mb-2">
+              @if (!empty($pricerate))
+              <a>เรทราคาของ Trip ปัจจุบัน = {{$pricerate}} ต่อระยะทาง 1 กิโลเมตร</a>
+                
+            @else
+            <a>เรทราคาของ Trip ปัจจุบัน = ยังไม่กำหนดเรทราคา ต่อระยะทาง 1 กิโลเมตร</a>
+            @endif
             
-        @else
-        <a>เรทราคาของ Trip ปัจจุบัน = ยังไม่กำหนดเรทราคา ต่อระยะทาง 1 กิโลเมตร</a>
-        @endif
-        
+            </div>
+            <div class="form-group mx-sm-3 mb-2">
+              <input type="number" class="form-control" id="priceRate" name="priceRate" placeholder="เรทราคาของทริปที่ต้องการแก้ไข">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">ยืนยัน</button>
+          </form>
         </div>
-        <div class="form-group mx-sm-3 mb-2">
-          <input type="number" class="form-control" id="priceRate" name="priceRate" placeholder="เรทราคาของทริปที่ต้องการแก้ไข">
+    
+        <div class="">
+          <form class="form-inline" style="margin-bottom: 5px !important;" method="POST" action="{{url('/tripprice')}}">
+            {{csrf_field()}}
+            <div class="form-group mb-2">
+              @if (!empty($pricerate))
+              <a>เรทราคาค่ามัดจำของ Trip ปัจจุบัน = {{$pricerate}} ต่อระยะทาง 1 กิโลเมตร</a>
+                
+            @else
+            <a>เรทราคาค่ามัดจำของ Trip ปัจจุบัน = ยังไม่กำหนดเรทราคา ต่อระยะทาง 1 กิโลเมตร</a>
+            @endif
+            
+            </div>
+            <div class="form-group mx-sm-3 mb-2">
+              <input type="number" class="form-control" id="priceRate" name="priceRate" placeholder="เรทราคาของทริปที่ต้องการแก้ไข">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">ยืนยัน</button>
+          </form>
         </div>
-        <button type="submit" class="btn btn-primary mb-2">ยืนยันการแก้ไข</button>
-      </form>
+    
+        <div class="">
+          <form class="form-inline" style="margin-bottom: 5px !important;" method="POST" action="{{url('/tripprice')}}">
+            {{csrf_field()}}
+            <div class="form-group mb-2">
+              @if (!empty($pricerate))
+              <a>ราคาเริ่่มต้นหลังสี่ทุ่มของ Trip ปัจจุบัน = {{$pricerate}} ต่อระยะทาง 1 กิโลเมตร</a>
+                
+            @else
+            <a>ราคาเริ่่มต้นหลังสี่ทุ่มของ Trip ปัจจุบัน = ยังไม่กำหนดเรทราคา ต่อระยะทาง 1 กิโลเมตร</a>
+            @endif
+            
+            </div>
+            <div class="form-group mx-sm-3 mb-2">
+              <input type="number" class="form-control" id="priceRate" name="priceRate" placeholder="เรทราคาของทริปที่ต้องการแก้ไข">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">ยืนยัน</button>
+          </form>
+        </div>
+    
+      </div>
     </div>
+
+    <div class="price-rate-card bg-light" style="margin-top: 20px;">
+      <div class="card-body text-center">
+        <p class="price-rate-title">เรทราคา Trip ส่งของ</p>
+        <div class="">
+          <form class="form-inline" style="margin-bottom: 5px !important;" method="POST" action="{{url('/tripprice')}}">
+            {{csrf_field()}}
+            <div class="form-group mb-2">
+              @if (!empty($pricerate))
+              <a>เรทราคาของ Trip ปัจจุบัน = {{$pricerate}} ต่อระยะทาง 1 กิโลเมตร</a>
+                
+            @else
+            <a>เรทราคาของ Trip ปัจจุบัน = ยังไม่กำหนดเรทราคา ต่อระยะทาง 1 กิโลเมตร</a>
+            @endif
+            
+            </div>
+            <div class="form-group mx-sm-3 mb-2">
+              <input type="number" class="form-control" id="priceRate" name="priceRate" placeholder="เรทราคาของทริปที่ต้องการแก้ไข">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">ยืนยัน</button>
+          </form>
+        </div>
+    
+        <div class="">
+          <form class="form-inline" style="margin-bottom: 5px !important;" method="POST" action="{{url('/tripprice')}}">
+            {{csrf_field()}}
+            <div class="form-group mb-2">
+              @if (!empty($pricerate))
+              <a>เรทราคาค่ามัดจำของ Trip ปัจจุบัน = {{$pricerate}} ต่อระยะทาง 1 กิโลเมตร</a>
+                
+            @else
+            <a>เรทราคาค่ามัดจำของ Trip ปัจจุบัน = ยังไม่กำหนดเรทราคา ต่อระยะทาง 1 กิโลเมตร</a>
+            @endif
+            
+            </div>
+            <div class="form-group mx-sm-3 mb-2">
+              <input type="number" class="form-control" id="priceRate" name="priceRate" placeholder="เรทราคาของทริปที่ต้องการแก้ไข">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">ยืนยัน</button>
+          </form>
+        </div>
+    
+        <div class="">
+          <form class="form-inline" style="margin-bottom: 5px !important;" method="POST" action="{{url('/tripprice')}}">
+            {{csrf_field()}}
+            <div class="form-group mb-2">
+              @if (!empty($pricerate))
+              <a>ราคาเริ่่มต้นหลังสี่ทุ่มของ Trip ปัจจุบัน = {{$pricerate}} ต่อระยะทาง 1 กิโลเมตร</a>
+                
+            @else
+            <a>ราคาเริ่่มต้นหลังสี่ทุ่มของ Trip ปัจจุบัน = ยังไม่กำหนดเรทราคา ต่อระยะทาง 1 กิโลเมตร</a>
+            @endif
+            
+            </div>
+            <div class="form-group mx-sm-3 mb-2">
+              <input type="number" class="form-control" id="priceRate" name="priceRate" placeholder="เรทราคาของทริปที่ต้องการแก้ไข">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">ยืนยัน</button>
+          </form>
+        </div>
+    
+      </div>
+    </div>
+    
 </div>
+
+
 
 
 </div>
--- resources/views/view_menu/usersview.blade.php
+++ resources/views/view_menu/usersview.blade.php
@@ -9,8 +9,9 @@
     @include('leftmenu')
   </div>
 
-  <div class="col-md-10 shadow p-3  rounded">
+  <div class="col-md-10 shadow p-3 rounded ">
 
+    
     <nav class="navbar navbar-light shadow" style="background-color: #b8e8ee;margin-bottom: 1em;">
       <a class="navbar-brand">ผู้ใช้งานทั่วไป</a>
 
@@ -22,61 +23,68 @@
 
     </nav>
 
-    @php
-    // dd($allusers);
-    @endphp
-    <table class="table" id="myTable">
-      <thead>
-        <tr>
-          <th scope="col" class="text-left">รูปโปรไฟล์</th>
-          <th scope="col" class="text-left">ชื่อ-นามสกุล</th>
-          <th scope="col" class="text-left">เบอร์ติดต่อ</th>
-          {{-- <th scope="col" class="text-left">ระดับ</th> --}}
+
+    <div class=" all-scrolling">
+      <div class="card-body">
+        <table class="table " id="myTable" style="justify-content: flex-start;">
+          <thead>
+            <tr>
+              <th scope="col" class="text-left">รูปโปรไฟล์</th>
+              <th scope="col" class="text-left">ชื่อ-นามสกุล</th>
+              <th scope="col" class="text-left">เบอร์ติดต่อ</th>
+              {{-- <th scope="col" class="text-left">ระดับ</th> --}}
+    
+    
+              <th scope="col" class="text-center">การจัดการ</th>
+            </tr>
+          </thead>
+    
+          <tbody>
+    
+            @foreach($allusers as $data)
+            @php
+            $id = $data['_id'];
+            @endphp
+            <tr>
+    
+              <td><a data-fancybox="gallery" href="{{$data->userPicture == null ? asset('images/system/nophoto.png'): $data->userPicture}}"><img
+                src="{{$data->userPicture == null ? asset('images/system/nophoto.png'): $data->userPicture}}" width="150px" height="100px"></a></td>
+              
+              <td>{{$data->userName}}</td>
+              <td>{{$data->phoneNumber}}</td>
+    
+    
+    
+    
+              <td class="layout-text-center">
+                
+                <form method="GET">
+                  {{csrf_field()}}
+    
+                  <a type="submit" href="{{action('AdminviewController@usersdetail',"id=$id")}}">
+    
+                    <i class="" aria-hidden="true">รายละเอียด</i>
+                  </a>
+                </form>
+    
+              </td>
+            </tr>
+    
+    
+    
+            @endforeach
+    
+          </tbody>
+    
+    
+        </table>
 
 
-          <th scope="col" class="text-center">การจัดการ</th>
-        </tr>
-      </thead>
+      </div>
+    </div>
+  
 
-      <tbody>
-
-        @foreach($allusers as $data)
-        @php
-        $id = $data['_id'];
-        @endphp
-        <tr>
-
-          <td><a data-fancybox="gallery" href="{{$data->userPicture == null ? asset('images/system/nophoto.png'): $data->userPicture}}"><img
-            src="{{$data->userPicture == null ? asset('images/system/nophoto.png'): $data->userPicture}}" width="150px" height="100px"></a></td>
-          
-          <td>{{$data->userName}}</td>
-          <td>{{$data->phoneNumber}}</td>
-
-
-
-
-          <td class="layout-text-center">
-            
-            <form method="GET">
-              {{csrf_field()}}
-
-              <a type="submit" href="{{action('AdminviewController@usersdetail',"id=$id")}}">
-
-                <i class="" aria-hidden="true">รายละเอียด</i>
-              </a>
-            </form>
-
-          </td>
-        </tr>
-
-
-
-        @endforeach
-
-      </tbody>
-
-
-    </table>
+    
   </div>
   <div>
 
